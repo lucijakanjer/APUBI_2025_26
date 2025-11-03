dca49a4824b0c7dcb133db83d94d7c3c98db4285
lucijakanjer <40788855+lucijakanjer@users.noreply.github.com>
2025-11-03 22:30:09 +0100

    Add files via upload

---
+++ 05_Deskriptivna_statistika/ogledni_zadatak_1.R
@@ -0,0 +1,41 @@
+# Analize podataka u biološkim istraživanjima - praktikum 2025/26
+# Lucija Kanjer lucija.kanjer@biol.pmf.hr
+
+###############################################################################
+
+# Ogledni zadatak za Kolokvij 1
+
+###############################################################################
+
+# Upute: 
+# Kod za rješenje svakog zadatka mora biti napisan u ovoj skripti ispod tog zadatka.
+# Kod mora biti napisan jedan ispod drugoga po redoslijedu izvršavanja.
+# Suvišan (krivi) kod mora biti izbrisan.
+# Sve naredbe koje možete, napišite preko koda, a ne klikanjem npr. postavljanje radnog direktorija.
+
+###############################################################################
+
+# CILJ: U setu podataka o pingvinima ("pingvini.xlsx") napravite usporedbu MASE pingvina po VRSTAMA!
+
+# 1. Napravite novu mapu naziva "samostalni_zadatak" unutar svoje postojeće mape i postavite ju kao radni direktorij.
+
+# 2. Učitajte tablicu u radno okruženje i pogledajte podatke.
+
+# 3. Napravite tablicu koja sadrži samo podatke o vrsti i masi pingvina.
+
+# 4. Grafički prikažite broj pingvina svake vrste. Koristite odgovarjući graf za prikaz kategoričke varijable, obojajte po vrsti i napišite osi na hrvatskom jeziku.
+
+# 5. Napravite histogram za masu pingvina svake vrste. Neka budu prikazani na jednom plotu.
+
+# 6. Opišite kako izgleda distribucija podataka o masi svake vrste s obzirom na simetričnost i nagnutost.
+
+# 7. Izračunajte deskriptivnu statistiku za masu (min, max, Q1, Q3, medijan i aritmetička sredina) svake vrste pingvina.
+
+# 8. Izračunajte standardnu devijaciju i standardnu pogrešku mase za svaku vrstu pingvina.
+
+# 9. S obzirom izgled distribucije, je li bolje koristiti aritmetičku sredinu ili medijan za prikaz centralne tendencije mase?
+
+# 10. Grafički usporedite mase između vrsta boxplot prikazom s dodanom oznakom aritmetičke sredine .
+
+# 11. Na temelju SVIH odrađenih grafika i izračuna deskriptivne statistike opišite i protumačite kako se ponaša masa pingvina za svaku vrstu u ovom istraživanju! 
+# *Pazite da donositie zaključke za ovaj uzorak pingvina, a ne za populaciju općenito!*
